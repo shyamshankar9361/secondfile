5056e07296df59835cdd4871c7cdf283dae1cd5f
shyam <shyamshankar9361@gmail.com>
2023-11-16 11:37:52 +0530

    changed

--- shyam.py
+++ shyam.py
@@ -1,6 +1,7 @@
 a=101
 b=22
 c=a+b
+d=85
 print("a*b =",a*b)
 if(a==12):
     print("hai")
@@ -8,6 +9,8 @@ elif(a==25):
     print("shyam")
 elif(a==b):
     print("hari")
+elif(d==55):
+    print("rsrs")    
 else:
     print("hello") 
 
